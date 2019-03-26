1cfebb2ba0fa3a24c4e69a6eb26565cc848c34fb
rudeboybert <albert.ys.kim@gmail.com>
2019-03-26 18:47:30 -0400

    Added example.

--- docs/static/create_binary_example_csv.R
+++ docs/static/create_binary_example_csv.R
@@ -22,3 +22,42 @@ example <- profiles %>%
   sample_n(2000)
 
 write_csv(example, path = here("static", "binary_example.csv"))
+
+
+library(reprex)
+
+library(tidyverse)
+library(yardstick)
+
+data("two_class_example")
+roc_auc(two_class_example, truth, Class1)
+
+
+
+roc_curve(two_class_example, truth, Class1) %>%
+  autoplot()
+
+two_class_example <- two_class_example %>%
+  as_tibble()
+
+two_class_example %>%
+  roc_auc(truth = truth, Class1)
+
+two_class_example <- two_class_example %>%
+  mutate(truth = fct_relevel(truth, "Class2"))
+
+two_class_example %>%
+  select(-Class2) %>%
+  mutate(Class1 = 1-Class1) %>%
+  roc_auc(truth = truth, Class1)
+
+
+values <- read_csv("https://rudeboybert.github.io/SDS293/static/methods/ROC/example.csv") %>%
+  mutate(y = factor(y))
+
+roc_curve(values, truth = y, p_hat) %>%
+  autoplot()
+
+values <- values %>%
+  mutate(y = fct_relevel(y, "0"))
+
