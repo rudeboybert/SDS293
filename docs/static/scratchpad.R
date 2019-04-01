f4aa46922212d375d483482b0eaed4a85a769fee
rudeboybert <albert.ys.kim@gmail.com>
2019-04-01 09:23:35 -0400

    DataFest participants

---
+++ docs/static/scratchpad.R
@@ -0,0 +1,3 @@
+library(tidyverse)
+library(googledrive)
+library(googlesheets)
---
+++ static/scratchpad.R
@@ -0,0 +1,32 @@
+library(tidyverse)
+library(janitor)
+library(googledrive)
+library(googlesheets)
+
+googledrive::drive_find(n_max = 25)
+
+datafest_participants_orig <-
+  "1A6v_1RskgVDiCBMIHV0ORt2cRSEg19_ntRL6-TwqqkY" %>%
+  gs_key() %>%
+  gs_read(ws = "2019 Participants") %>%
+  mutate(Email = tolower(Email))
+
+datafest_participants <- datafest_participants_orig %>%
+  filter(Institution == "Smith") %>%
+  select(X3, `Last Name`, Email, Withdrew)
+
+ML_students_orig <-
+  "1GqeIpPxyr9Q29VgdZUn2t7cdP3XeUN4_VI833o2xUno" %>%
+  gs_key() %>%
+  gs_read(ws = "Roster") %>%
+  mutate(Email = tolower(Email))
+
+ML_students <-
+  ML_students_orig %>%
+  select(`First name`, `Last name`, Email) %>%
+  left_join(datafest_participants, by = "Email") %>%
+  filter(!is.na(X3)) %>%
+  filter(`Last name` != "Mudanye", `Last name` != "Simplice", `Last name` != "Nawaz") %>%
+  clean_names() %>%
+  select(first_name, last_name) %>%
+  arrange(first_name)
